616aee3c969ea9ec87b4e3e3dc6fe2879126d7f4
niedongjia <327803131@qq.com>
2020-06-16 12:39:36 +0800

    <feat>:<基础框架构建>

---
+++ et-monitor-platform-app/et-monitor-platform-app-common/src/main/java/com/zntie/cn/etmp/App.java
@@ -0,0 +1,13 @@
+package com.zntie.cn.etmp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ et-monitor-platform-iot/et-monitor-platform-iot-mbs/src/test/java/com/zntie/cn/etmp/AppTest.java
@@ -0,0 +1,20 @@
+package com.zntie.cn.etmp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
